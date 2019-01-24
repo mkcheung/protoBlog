e9d1382431babc58dcc1358d71f2855994d5b67e
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-23 18:36:26 -0800

    initial load

---
+++ app/Http/Controllers/BlogsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blog;
+use Illuminate\Http\Request;
+use function var_dump;
+
+class BlogsController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::all();
+        return view('blogs.index', compact('blogs'));
+    }
+
+    public function create()
+    {
+        return view('blogs.create');
+    }
+
+    public function store(Request $request)
+    {
+//        $blog = new Blog();
+//        $blog->title = $request->title;
+//        $blog->body = $request->body;
+//        $blog->save();
+
+        $input = $request->all();
+        $blog = Blog::create($input);
+
+        return redirect('/blogs');
+
+    }
+}
---
+++ resources/views/blogs/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="div.container-fluid">
+        <div class="jumbotron">
+            <h1>Create a new blog</h1>
+        </div>
+        <div class="col-md-12">
+            <form action="{{route('blogs.store')}}" method="post">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control"></input>
+                </div>
+                <div class="form-group">
+                    <label for="body">Body</label>
+                    <input name="body" class="form-control"></input>
+                </div>
+                <button class="btn btn-primary" type="submit">Create a new blog</button>
+                {{csrf_field()}}
+            </form>
+        </div>
+        <div class="jumbotron">
+            <h1>Create a new blog</h1>
+        </div>
+    </div>
+
+
+@endsection
