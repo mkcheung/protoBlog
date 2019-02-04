f1f43d8dfd69465371b1d2c40ef1743f30597fde
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-02-04 14:43:09 -0800

    tweak css for blog - compile css with webpack

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -4,18 +4,29 @@
 
     <div class="div.container-fluid">
         <article>
+
             <div class="jumbotron">
-                <h1>{{ $blog->title }}</h1>
-                <a class="btn btn-primary btn-xs pull-left" href="{{route('blogs.edit', $blog->id)}}">Edit</a>
-                <form method="post" action="{{route('blogs.delete', $blog->id)}}">
-                    {{ method_Field('delete') }}
-                    <button type="submit" class="btn btn-danger btn-xs pull-left">Delete</button>
-                    {{csrf_field()}}
-                </form>
+
+                <div class="col-md-12">
+                    <h1>{{ $blog->title }}</h1>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="btn-group">
+                        <a class="btn btn-primary btn-xs pull-left btn-margin-right" href="{{route('blogs.edit', $blog->id)}}">Edit</a>
+                        <form method="post" action="{{route('blogs.delete', $blog->id)}}">
+                            {{ method_Field('delete') }}
+                            <button type="submit" class="btn btn-danger btn-xs pull-left">Delete</button>
+                            {{csrf_field()}}
+                        </form>
+                    </div>
+                </div>
             </div>
+
             <div class="col-md-12">
                 <p>{{ $blog->body }}</p>
             </div>
+
         </article>
     </div>
 
